9407c6aad90f8a31d7471bc7c432a50d36f4aa20
thanhlong1806 <36601329+zzphantomz@users.noreply.github.com>
2024-05-05 07:41:30 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['three'],
+    eslint: {
+
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
